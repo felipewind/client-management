c61d37bf6930a931d905810cafb1b408589f3414
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2021-03-10 22:05:26 -0300

    Revert "first version (#19)" (#20)
    This reverts commit e9066ba529e3124bede68f4f160c1732c7db757d.

--- back-end/src/test/java/com/helesto/endpoint/ClientEndpointTestIT.java
+++
@@ -1,9 +0,0 @@
-package com.helesto.endpoint;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class ClientEndpointTestIT extends ClientEndpointTest {
-
-    // Execute the same tests but in native mode.
-}
