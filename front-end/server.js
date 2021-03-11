9a02f6ef7cf09b9945d3911a5cfd66efeddfb7c6
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2021-03-10 22:05:10 -0300

    Revert "first version (#19)"
    This reverts commit e9066ba529e3124bede68f4f160c1732c7db757d.

--- front-end/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.use("/", express.static("./dist/front-end"));
-
-app.listen(80, () => console.log("HTTP Server running on port 80"));
-
