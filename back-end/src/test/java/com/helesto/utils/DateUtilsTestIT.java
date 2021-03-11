af5e0b092b6f0c032951694ad73c14b1d288f39a
Felipe Henrique Gross Windmoller <felipewind@gmail.com>
2021-03-10 21:59:28 -0300

    Revert "Develop (#17)" (#18)
    This reverts commit cfb5943a54cb1733fa94be468bf815e57f3e3dfa.

--- back-end/src/test/java/com/helesto/utils/DateUtilsTestIT.java
+++
@@ -1,9 +0,0 @@
-package com.helesto.utils;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-class DateUtilsTestIT extends DateUtilsTest  {
-
-    // Execute the same tests but in native mode.
-}
